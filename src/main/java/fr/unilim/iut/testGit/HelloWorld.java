9d72c633559d12656bb6696f365e797414354246
Alexander Kurth <alexander.kurth@etu.unilim.fr>
2018-05-01 19:00:54 +0200

    resolition conflict

--- src/main/java/fr/unilim/iut/testGit/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testGit/HelloWorld.java
@@ -2,19 +2,19 @@ package fr.unilim.iut.testGit;
 
 public class HelloWorld {
 	
-    public static void main( String[] args )
-        {
-			System.out.println( "Bonjour tout le monde!" );  
-		System.out.println( "Jouons ensemble!" );  
-		
-		System.out.println( "Et si on jouait au FizzBuzz ?" );  
+    public static void main(String[] args) 
+    {
+	    System.out.println( "Hello World !" );
+	    System.out.println( "Jouons ensemble!" );
+	
+	    System.out.println( "Et si on jouait au FizzBuzz ?" );  
         System.out.println(FizzBuzz.jouer());
-
-		System.out.println( "Et si on jouait au Marabout ?" );  
+    
+        System.out.println( "Et si on jouait au Marabout ?" );  
         System.out.println(Marabout.jouer());
+    
+        System.out.println( "Et si on jouait au Marabout de manière plus simple ?" );  
+        System.out.println(MaraboutSimple.getResult());
+}
 
-		System.out.println( "Et si on jouait au Marabout de manière plus simple ?" );  
-        System.out.println(MaraboutSimple.jouer());
-    }
-}  
-
+}
