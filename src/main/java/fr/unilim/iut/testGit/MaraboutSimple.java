9ae5fbfe433375bb887edb8ad5b38d8554c17a32
Alexander Kurth <alexander.kurth@etu.unilim.fr>
2018-05-01 18:54:11 +0200

    Ajout Marabout simple 3 mots de plus

--- src/main/java/fr/unilim/iut/testGit/MaraboutSimple.java
+++ src/main/java/fr/unilim/iut/testGit/MaraboutSimple.java
@@ -2,9 +2,10 @@ package fr.unilim.iut.testGit;
 
 
 public class MaraboutSimple {
-
+	
     public static String getResult() {
-       return "Marabout, Bout de ficelle, Selle de cheval";
+       return "Marabout, Bout de ficelle, Selle de cheval"
+              + ", Cheval de course, Course à pied, Pied à terre";
  }
 
-}
+} 
