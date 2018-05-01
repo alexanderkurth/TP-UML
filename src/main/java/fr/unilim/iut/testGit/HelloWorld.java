71f479e103ac6eedd0331239247c17f1e9bab274
Alexander Kurth <alexander.kurth@etu.unilim.fr>
2018-05-01 18:39:50 +0200

    Ajout FizzBuzz de 1 à 15.

--- src/main/java/fr/unilim/iut/testGit/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testGit/HelloWorld.java
@@ -1,10 +1,13 @@
 package fr.unilim.iut.testGit;
 
 public class HelloWorld {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello World ! ");
-		System.out.println( "Jouons ensemble!" );
-	}
-
-}
+    	
+	    public static void main( String[] args )
+            {
+ 			System.out.println( "Hello World!" );
+    		System.out.println( "Jouons ensemble!" );  
+			
+			System.out.println( "Et si on jouait au FizzBuzz ?" );  
+            System.out.println(FizzBuzz.getResult());
+        }
+    }  
